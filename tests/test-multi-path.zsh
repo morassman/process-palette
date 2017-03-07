8ba0d62d1b8ae3674ae03d357aacc8187a8ec911
hg42@gmx.net <hg42@gmx.net>
2017-03-07 17:36:24 +0100

    add test scripts

---
+++ tests/test-multi-path.zsh
@@ -0,0 +1,3 @@
+
+echo "-- blafusel test-multi-path.zsh:1 hurzmiburz test-multi-path.zsh:2 ERROR foo test-multi-path.zsh:3 ERROR bar"
+echo "xx blafusel ERROR test-multi-path.zsh:1 hurzmiburz test-multi-path.zsh:2 foo test-multi-path.zsh:3 bar"
