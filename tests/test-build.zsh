53c60eb3e1c15cdc617e220950b7ade636b18296
hg42@gmx.net <hg42@gmx.net>
2017-03-17 15:54:57 +0100

    more tests

--- tests/test-build.zsh
+++ tests/test-build.zsh
@@ -3,3 +3,6 @@ echo
 echo "build: at: /usr/local/src/atom/packages/process-palette/lib/views args: /usr/local/src/atom/packages/process-palette/lib/views/process-output-view.coffee"
 echo "build: at: ... args: /usr/local/src/atom/packages/process-palette/lib/views/process-output-view.coffee"
 echo "xxxx.xxx /usr/local/src/atom/packages/process-palette/lib/views/process-output-view.coffee"
+echo "BUILD FAILED"
+echo "build: compilation error"
+echo "   * TEST"
