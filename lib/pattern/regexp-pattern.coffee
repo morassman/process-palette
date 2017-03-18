226a2fbea407b5edde1eccffda419be401d00e23
hg42@gmx.net <hg42@gmx.net>
2017-03-19 00:32:43 +0100

    replace workaround by working solution

--- lib/pattern/regexp-pattern.coffee
+++ lib/pattern/regexp-pattern.coffee
@@ -11,25 +11,23 @@ class RegExpPattern
 
     if !matches?
       return null;
-      
+
     try
-      if matches.length > 1
-        ## we have found at least one group, use it as match
-        #console.log(["match1", matches])
-        match = matches[1]
-        # this is a workaround, not absolutely correct!
-        # javascript does not have index of a regex group
-        # so find matched group in complete match and add offset
-        # this does not work if the group string occurs multiple times in matches[0]
-        # and the regex does not select the first occurance (e.g. might be anchored at $)
-        start = matches.index + matches[0].indexOf(match)
+      #console.log(["matches", matches])
+      if matches.length == 4 && (matches[1].length + matches[2].length + matches[3].length == text.length)
+        ## we have a special pattern with three groups for pre, match, post
+        # (note: three groups are necessary, because javascript does not have index of a group matches)
+        #console.log(["matches3", matches])
+        pre = matches[1]
+        match = matches[2]
+        post = matches[3]
       else
         ## no group, use complete match
         match = matches[0]
         start = matches.index
-      end = start + match.length
-      pre = text.substring(0, start);
-      post = text.substring(end);
+        end = start + match.length
+        pre = text.substring(0, start);
+        post = text.substring(end);
       #console.log(["pre/match/post", pre, match, post])
       return new RegExpMatch(text, match, pre, post);
 
--- tests/test-look-behind.zsh
+++ tests/test-look-behind.zsh
@@ -16,36 +16,31 @@
 # line starting with / regex matches " = some content but not the last word"
 #
 # "trace-result": {
-#   "expression": "(?:^\\s*/\\s+.*)(\\s+=\\s+.*)(?:\\s+\\w+)$"
+#   "expression": "(^\\s*/\\s+.*)(\\s+=\\s+.*)(.*$)"
 # },
 #
 # also after the above sections an expression matching the fords fail, failed or failure
 #
 # "fail": {
-#   "expression": "\\b(fail(ure|ed)?)\\b"
+#   "expression": "\\bfail(ure|ed)?\\b"
 # },
 
 echo "some unstyled text"
-# should mark next two lines as .trace-begin
+# should style next two lines as .trace-begin
 echo "   \ a ( x=1 y=2 z=3 )"
 echo "     \ b ( x=6 )"
-# should style the line as .trace-end
-# also .trace-result starting with " = ", but not xxx
-echo "     / b ( x=6 ) = 'converted to string: 6' xxx"
-# should style the line as .trace-end
-# no .trace-result because no word at end of line
-#     (" 6'" is NOT a word, because "'" is not included in \w)
+# should style next line as .trace-end and .trace-result starting with " = "
+echo "     / b ( x=6 ) = 'converted to string: 6'"
+# should style next line as .trace-end and .trace-result starting with " = "
 echo "   / a ( x=1 y=2 z=3 ) = 'converted to string: 6'"
 echo "unstyled text inbetween"
-# fictional case for multiple .trace-result " = result",
-# because of the javascript workaround incorrectly only the first will be styled,
-# but the regex is for the second = xxx
-echo " / example = xxx = xxx zzz"
-# here correctly " = yyy" is styled because both group matches differ
-echo " / example = xxx = yyy zzz"
+# fictional case for multiple .trace-result " = xxx", the second should be styled
+echo " / example = xxx = xxx"
+# " = yyy" should be styled
+echo " / example = xxx = yyy"
 # " = yyy" is styled, but "failure" is not, because the pattern it is not
 # applied to pre and post strings after detecting " = yyy"
-# if the sequence is "fail, trace-result" instead,
-#     then "failure" will be styled, but not " = yyy"
-echo " / failure example = xxx = yyy zzz"
+# if the sequence would be "fail, trace-result" instead,
+#     then "failure" would be styled, but not " = yyy"
+echo " / failure example = xxx = yyy"
 echo "some more unstyled text"
